7c3abc09ce1338691dd91d7ae8a9d7bdc18c3379
Kaito-Kawarada <mmrtjl.kaito@gmail.com>
2022-10-01 16:43:47 +0900

    ()内のステータス修正7→8

--- keyword_explorer_pd.py
+++ keyword_explorer_pd.py
@@ -4,7 +4,8 @@ import matplotlib
 matplotlib.rc('font', family='BIZ UDGothic')
 ctv_list=list()
 date_list=str()
-df=pd.read_csv('tokyo_2021syazyounerai.csv', skiprows=[0,1676], encoding="shift_jis", names=["罪名","手口","管轄警察署（発生地）","管轄交番・駐在所（発生地）","市区町村コード（発生地）","都道府県（発生地）","市区町村（発生地）","町丁目（発生地）","発生年月日（始期）","発生時（始期）","発生場所","発生場所の詳細","施錠関係","現金被害の有無"])
+#df=pd.read_csv('tokyo_2021syazyounerai.csv', skiprows=[-1], encoding="shift_jis", names=["罪名","手口","管轄警察署（発生地）","管轄交番・駐在所（発生地）","市区町村コード（発生地）","都道府県（発生地）","市区町村（発生地）","町丁目（発生地）","発生年月日（始期）","発生時（始期）","発生場所","発生場所の詳細","施錠関係","現金被害の有無"])
+df=pd.read_csv('tokyo_2021syazyounerai.csv', encoding="shift_jis")
 ctv_list=df.loc[:,"市区町村（発生地）"]
 date_list=pd.to_datetime(df.loc[:,"発生年月日（始期）"])
 print(ctv_list.value_counts())
