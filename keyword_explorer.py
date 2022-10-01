790bc9ca2c7006b8ce711fb633ec83fc0dda3f45
Kaito-Kawarada <mmrtjl.kaito@gmail.com>
2022-10-01 11:55:02 +0900

    collections.Counterを使いました

--- keyword_explorer.py
+++ keyword_explorer.py
@@ -1,14 +1,12 @@
 import csv
+from collections import Counter
 
-CTVcount_list=list()
 CTV_list=list()
-preCTV='o'
+l=list()
 with open('tokyo_2021syazyounerai.csv','r', encoding = "Shift-JIS") as read_file:
-    count=1
-     for text in csv.reader(read_file):
-        if (preCTV != text[6] && CTV_list.find(text) >= 1):
-            CTVcount_list.append(count)
-            preCTV=text[6]
-        else:
-            count += 1
-
+    for text in csv.reader(read_file):
+        CTV_list.append(text[6])
+        l=Counter(CTV_list)
+del l['市区町村（発生地）']
+del l['']
+print(l)
