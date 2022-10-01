58a6a375a60059f017dc835a2b792ac44f3adf35
Kaito-Kawarada <mmrtjl.kaito@gmail.com>
2022-10-01 13:12:45 +0900

    変数名を修正

--- keyword_explorer.py
+++ keyword_explorer.py
@@ -1,12 +1,12 @@
 import csv
 from collections import Counter
 
-CTV_list=list()
-l=list()
+ctv_list=list()
+ctv_countlist=list()
 with open('tokyo_2021syazyounerai.csv','r', encoding = "Shift-JIS") as read_file:
     for text in csv.reader(read_file):
-        CTV_list.append(text[6])
-        l=Counter(CTV_list)
-del l['市区町村（発生地）']
-del l['']
-print(l)
+        ctv_list.append(text[6])
+ctv_countlist=Counter(ctv_list)
+del ctv_countlist['市区町村（発生地）']
+del ctv_countlist['']
+print(ctv_countlist)
